dc45347dc11b946c7d081cfe17010333040eca13
Kavyan Nayak <kavyannayak104@gmail.com>
2026-01-12 20:46:09 +0530

    Basics

--- basics/1.js
+++ basics/1.js
@@ -1,11 +1,30 @@
 // External Js
+// variable declaration  : C -> int char float double
 
+        // js -> var , let , const
+
+// var : it use dynamically
+// let : it use for block scope
+// const :it use for constant value
+
+
+// dynamic means we can change the value and data type also   int -> string
+
+// block scope : it is use for only in the block {  }
+
+// const : it is use for constant value we cant change the value
+
+
+// declaration 
+
+// var variable_name = 10;  
 
 var a = 10;
-var b = 'kavyan';
-
-console.log( "The Value of A is :", a);
+var b = "hello";
 
 
-console.log( "Your Name is :", b);
+// c -> printf   // py -> print   // js -> console.log()
 
+
+console.log(a);
+console.log(b);
