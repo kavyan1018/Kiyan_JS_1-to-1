70140ba45ebf3fb1d48760dfccb433408cff5f74
Kavyan Nayak <kavyannayak104@gmail.com>
2026-06-28 14:41:00 +0530

    task

--- Js/time.js
+++ Js/time.js
@@ -2,7 +2,7 @@ let a = prompt("Enter the Seconds :");
 
 let hours = a / 3600;
 let minutes = (a % 3600) / 60;
-let seconds = a % 60;
+let seconds = a % 3600 % 60;
 
 document.write(hours)
 document.write(minutes)
